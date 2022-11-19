ecbb0cb987a1c6c8293c2602faed8072c8741a1b
ianRickmers <ian.rickmers@usach.cl>
2022-11-19 20:46:35 -0300

    dockerfile

--- src/main/java/edu/migsw/inasistencia/InasistenciaApplication.java
+++ src/main/java/edu/migsw/inasistencia/InasistenciaApplication.java
@@ -2,8 +2,10 @@ package edu.migsw.inasistencia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InasistenciaApplication {
 
 	public static void main(String[] args) {
